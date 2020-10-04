4e8cb8942768660c503207152a4a3c4c880ea0e9
Giovani Florek <giovani.florek@promob.com>
2020-10-04 04:01:47 -0300

    cjamges

--- src/app/pages/games/sueca/player.ts
+++ src/app/pages/games/sueca/player.ts
@@ -1,5 +1,6 @@
 export class Player {
     public name: string;
+    public numberInTheOrder: number;
     public gender?: string;
     public age?: number;
     public status?: string;
--- src/app/pages/games/sueca/sueca.page.ts
+++ src/app/pages/games/sueca/sueca.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit,NgModule } from '@angular/core';
 import { Player } from './player';
 import { Cards } from './cards';
 @Component({
@@ -8,24 +8,97 @@ import { Cards } from './cards';
 })
 export class SuecaPage {
   public numberOfPlayers: number;
-  public players: Player[];
+  public players: Array<Player> = [];
   public cards: Cards;
+  public turn = 0;
+  public roundDuration = 0;
+  public cardSelected: number;
+  public playerSelected: Player;
+  public FirstTurn: boolean;
+  public LoaderIsActive: boolean;
 
-public CreatePlayers():void {
-  for(let index = 1; index+ 1 < this.numberOfPlayers + 1; index++) {
-  let player = new Player();
-  player.name = "Player " + index;
-  this.players.push(player);
-}
-}
+  public CreatePlayers(numberOfPlayers: number): void {
+    this.numberOfPlayers = numberOfPlayers;
+    const player = new Player();
+    for (let index = 0; index < this.numberOfPlayers; index++) {
+      player.name = 'Player ' + index;
+      player.numberInTheOrder = index;
+      this.players.push(player);
+    }
+    console.log(this.players.length);
+  }
 
-public ChooseCard():number{
-  let number =
-  Math.floor((Math.random() * 52) + 1);
-  console.log(number);
- return number;
-}
+  public StartGame(event): void {
+    const numberOfPlayersSelected  = event.srcElement.innerHTML;
+    console.log(numberOfPlayersSelected);
+    this.RoundStart();
+    this.CreatePlayers(numberOfPlayersSelected);
+    this.ActiveLoader();
+    this.RollCard();
+    this.ActiveSelectionLoading();
+    this.IsFirstTurn();
+    this.GenerateRounds();
+    this.showPlayerCard(this.SelectPlayer());
+    this.showCard(this.cardSelected);
+  }
 
+  showCard(cardSelected: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  showPlayerCard(player: Player) {
+    throw new Error('Method not implemented.');
+  }
+
+  GenerateRounds() {
+    this.roundDuration = this.players.length;
+  }
+
+  SelectPlayer(): Player {
+    return this.playerSelected = this.players.find(p => p.numberInTheOrder == this.turn);
+
+  }
+
+  public RollCard(): void {
+    const value = this.GenerateNumber();
+    this.cardSelected = value;
+
+  }
+
+  public IsFirstTurn(): void {
+    if (this.turn > 0) {
+      this.FirstTurn = false;
+    }
+  }
+
+  ActiveSelectionLoading(): void {
+    this.ActiveLoader();
+  }
+
+  RoundStart() {
+    this.turn++;
+  }
+
+  ActiveLoader(): void {
+    this.LoaderIsActive = true;
+    setTimeout (() => {
+      this.UnactiveLoader();
+   }, 1000);
+  }
+
+  UnactiveLoader():void{
+    this.LoaderIsActive = false;
+  }
+
+  NumberSelected(number: number) {
+
+  }
+
+
+  public GenerateNumber(): number {
+    let number = Math.floor((Math.random() * 52) + 1);
+    return number;
+  }
 
 
 }
